35bf741092e6ef01ff9cc6a113c41a65d0c5d348
Yuval Yssak <yuval.yssak@winn.ai>
2024-04-02 20:57:10 +0300

    move into a single file

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,13 @@
-import { useReducer } from "react";
-import { ActionType, TaskType } from "./types";
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
-import { TasksContext, TasksDispatchContext } from "./TasksContext";
+import { TasksProvider } from "./TasksContext";
 
 export default function TaskApp() {
-    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-
     return (
-        <TasksContext.Provider value={tasks}>
-            <TasksDispatchContext.Provider value={dispatch}>
-                <h1>Day off in Kyoto</h1>
-                <AddTask />
-                <TaskList />
-            </TasksDispatchContext.Provider>
-        </TasksContext.Provider>
+        <TasksProvider>
+            <h1>Day off in Kyoto</h1>
+            <AddTask />
+            <TaskList />
+        </TasksProvider>
     );
 }
-
-function tasksReducer(tasks: TaskType[], action: ActionType) {
-    switch (action.type) {
-        case "add": {
-            return [
-                ...tasks,
-                {
-                    id: action.id,
-                    text: action.text,
-                    done: false,
-                },
-            ];
-        }
-        case "change": {
-            return tasks.map((t) => {
-                if (t.id === action.task.id) {
-                    return action.task;
-                } else {
-                    return t;
-                }
-            });
-        }
-        case "delete": {
-            return tasks.filter((t) => t.id !== action.id);
-        }
-    }
-}
-
-const initialTasks: TaskType[] = [
-    { id: 0, text: "Philosopher's Path", done: true },
-    { id: 1, text: "Visit the temple", done: false },
-    { id: 2, text: "Drink matcha", done: false },
-];
--- src/TasksContext.tsx
+++ src/TasksContext.tsx
@@ -1,5 +1,48 @@
-import { createContext } from "react";
+import { PropsWithChildren, createContext, useReducer } from "react";
 import { ActionType, TaskType } from "./types";
 
-export const TasksContext = createContext<TaskType[]>(null as unknown as TaskType[]);
-export const TasksDispatchContext = createContext<(action: ActionType) => void>(null as unknown as (action: ActionType) => void);
+const initialTasks: TaskType[] = [
+    { id: 0, text: "Philosopher's Path", done: true },
+    { id: 1, text: "Visit the temple", done: false },
+    { id: 2, text: "Drink matcha", done: false },
+];
+
+export const TasksContext = createContext(initialTasks);
+export const TasksDispatchContext = createContext<(action: ActionType) => void>(() => {});
+
+function tasksReducer(tasks: TaskType[], action: ActionType) {
+    switch (action.type) {
+        case "add": {
+            return [
+                ...tasks,
+                {
+                    id: action.id,
+                    text: action.text,
+                    done: false,
+                },
+            ];
+        }
+        case "change": {
+            return tasks.map((t) => {
+                if (t.id === action.task.id) {
+                    return action.task;
+                } else {
+                    return t;
+                }
+            });
+        }
+        case "delete": {
+            return tasks.filter((t) => t.id !== action.id);
+        }
+    }
+}
+
+export function TasksProvider({ children }: PropsWithChildren) {
+    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+    return (
+        <TasksContext.Provider value={tasks}>
+            <TasksDispatchContext.Provider value={dispatch}>{children}</TasksDispatchContext.Provider>
+        </TasksContext.Provider>
+    );
+}
